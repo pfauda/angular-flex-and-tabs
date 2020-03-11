f695cffa20082ce4c5080017ce012d873bb1370c
pfauda <pablo.fauda@gmail.com>
2020-03-11 18:21:31 -0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TopComponent } from './component/top/top.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TopComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
